d640bda065f0450c9b28543094d851b4ddfe9e53
Dushyant <dushyanty2005@gmail.com>
2025-12-14 16:09:51 +0530

    Implement additional pattern printing methods
    Added multiple pattern printing methods including right-angled number pyramids and inverted pyramids.

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-//*****SQUARE PATTERN*****//
+// 1. SQUARE PATTERN
 public class Main {
 
     public static void main(String[] args) {
@@ -18,7 +18,7 @@ public class Main {
     }
 } 
 
-//****RIGHT ANGLE TRIANGLE//
+// 2. RIGHT ANGLE TRIANGLE
 
  public class Main {
     public static void main(String[] args) {
@@ -33,3 +33,60 @@ public class Main {
         }
     }
 } 
+
+// 3. Right-Angled Number Pyramid
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();   
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+// 4. Right-Angled Number Pyramid - II
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();   // number of rows
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+// 5. Inverted Right Pyramid
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+
+
+
